0be5a7c918650b3ca703db002314ac6e64fb062f
1andyn <andy.work.dev@gmail.com>
2020-08-16 11:29:51 -1000

    gh-pages push

---
+++ precache-manifest.67b69927d76cccba47cff0bc18cb30ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f152758b092a64f80bb66ee6a14be893",
+    "url": "/pantry-hero-fe/index.html"
+  },
+  {
+    "revision": "95dcd8d1bfb43a7aec73",
+    "url": "/pantry-hero-fe/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "c025e5e900d66ac60fe9",
+    "url": "/pantry-hero-fe/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "95dcd8d1bfb43a7aec73",
+    "url": "/pantry-hero-fe/static/js/2.073964ca.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "/pantry-hero-fe/static/js/2.073964ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c025e5e900d66ac60fe9",
+    "url": "/pantry-hero-fe/static/js/main.89ba4ab5.chunk.js"
+  },
+  {
+    "revision": "3db2db32e3db6b6e5f4a",
+    "url": "/pantry-hero-fe/static/js/runtime-main.98c0a651.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/pantry-hero-fe/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pantry-hero-fe/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc36ecf5b912bc69075b43cfdeaa6db6.js"
+  "/pantry-hero-fe/precache-manifest.67b69927d76cccba47cff0bc18cb30ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pantry-hero-fe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
