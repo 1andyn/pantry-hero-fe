b728c6afcdeea02aa563211d2cd761240b288fb7
Joseph Savold <joesavold@hotmail.com>
2020-08-30 07:22:31 -1000

    Added SET pantry reducer
    Added set to allow us to set the whole ingredients state when we load.

--- src/actions/pantry.js
+++ src/actions/pantry.js
@@ -1,16 +1,23 @@
+function setIngredients(items) {
+    return {
+        type: "ADD",
+        state: items,
+    };
+}
+
 function addIngredient(item) {
     return {
-        type: 'ADD',
-        pantry_item: item
-    }
+        type: "ADD",
+        pantry_item: item,
+    };
 }
 
 function delIngredient(index) {
     /* May want to do search logic in here which will change parameters*/
     return {
-        type: 'DEL',
-        delete_idx: index
-    }
+        type: "DEL",
+        delete_idx: index,
+    };
 }
 
-export default {addIngredient, delIngredient};
+export default { addIngredient, delIngredient };
--- src/reducers/pantry.js
+++ src/reducers/pantry.js
@@ -1,18 +1,20 @@
 const pantryReducer = (state = [], action) => {
-    switch(action.type) {
-        case 'ADD': {
-            var state_copy = [...state];
-            state_copy.push(action.pantry_item);
+    switch (action.type) {
+        case "SET": {
+            return action.state;
+        }
+        case "ADD": {
+            var state_copy = [...state, action.pantry_item];
             return state_copy;
         }
-        case 'DEL': {
+        case "DEL": {
             var state_copy = [...state];
-            state_copy.splice(action.delete_idx,1);
+            state_copy.splice(action.delete_idx, 1);
             return state_copy;
         }
         default:
             return state; //do nothing return pantry
     }
-}
+};
 
 export default pantryReducer;
